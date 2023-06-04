64cc2fd3593bc92d76095742e272cffdb8396538
Agam Sahu <agamsahu017@gmail.com>
2023-06-05 01:01:45 +0530

    Create apis.js
    video db json data

---
+++ apis.js
@@ -0,0 +1,82 @@
+[
+  {
+    "id": 1,
+    "channel": "Martelle",
+    "views": 61,
+    "title": "Big Buck Bunny",
+    "time": 90,
+    "verified": false
+  },
+  {
+    "id": 2,
+    "channel": "Vivyanne",
+    "views": 80,
+    "title": "Crime Wave",
+    "time": 64,
+    "verified": false
+  },
+  {
+    "id": 3,
+    "channel": "Nikki",
+    "views": 75,
+    "title": "Daniel Deronda",
+    "time": 90,
+    "verified": false
+  },
+  {
+    "id": 4,
+    "channel": "Gunilla",
+    "views": 9,
+    "title": "Once Upon a Crime...",
+    "time": 7,
+    "verified": false
+  },
+  {
+    "id": 5,
+    "channel": "Merry",
+    "views": 16,
+    "title": "Fear",
+    "time": 1,
+    "verified": false
+  },
+  {
+    "id": 6,
+    "channel": "Faulkner",
+    "views": 34,
+    "title": "Elephant White",
+    "time": 24,
+    "verified": false
+  },
+  {
+    "id": 7,
+    "channel": "Bondy",
+    "views": 32,
+    "title": "The Sea Vultures",
+    "time": 8,
+    "verified": false
+  },
+  {
+    "id": 8,
+    "channel": "Emmalynn",
+    "views": 21,
+    "title": "How to Train Your Dragon",
+    "time": 96,
+    "verified": true
+  },
+  {
+    "id": 9,
+    "channel": "Yvon",
+    "views": 30,
+    "title": "Beneath Hill 60",
+    "time": 100,
+    "verified": true
+  },
+  {
+    "id": 10,
+    "channel": "Elle",
+    "views": 20,
+    "title": "Postman Blues",
+    "time": 93,
+    "verified": true
+  }
+]
